d601f6215429662db475e70a80d389808212a139
duytran <duyquangtran@kms-technology.com>
2024-10-10 07:14:37 -0500

    Test add more headers for Frontend request

--- website/src/App.js
+++ website/src/App.js
@@ -43,6 +43,7 @@ const Blog = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
           },
           body: JSON.stringify({
             query: searchQuery,
